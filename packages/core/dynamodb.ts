ac96235402fc668f12424e69f9a27b43f35c27b7
slackermorris <morris.w.jack@gmail.com>
2023-09-15 10:01:08 +1200

    feature: when creating a unique user, uniquely create a watchlist too

--- packages/core/dynamodb.ts
+++ packages/core/dynamodb.ts
@@ -2,8 +2,6 @@ import AWS from "aws-sdk";
 
 const client = new AWS.DynamoDB.DocumentClient();
 
-
-
 export default {
   get: (params: AWS.DynamoDB.DocumentClient.GetItemInput) =>
     client.get(params).promise(),
@@ -15,4 +13,8 @@ export default {
     client.update(params).promise(),
   delete: (params: AWS.DynamoDB.DocumentClient.DeleteItemInput) =>
     client.delete(params).promise(),
+  transaction: (params: AWS.DynamoDB.DocumentClient.TransactWriteItemsInput) =>
+    client.transactWrite(params).promise(),
+  batchWrite: (params: AWS.DynamoDB.DocumentClient.BatchWriteItemInput) =>
+    client.batchWrite(params).promise(),
 };
