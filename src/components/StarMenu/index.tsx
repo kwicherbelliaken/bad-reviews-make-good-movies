84d99d7eae52ea4dded41ae8871a6ee580be3c49
slackermorris <morris.w.jack@gmail.com>
2023-11-06 08:11:48 +1300

    feature: basic styling for the /watchlist link.. seems we have an animation bug

--- src/components/StarMenu/index.tsx
+++ src/components/StarMenu/index.tsx
@@ -58,7 +58,7 @@ const StarMenu = () => {
       className="absolute right-0 overflow-visible w-full h-full z-10 bg-transparent group"
     >
       <div
-        className={`absolute z-10 w-16 right-0 top-0 m-8 cursor-pointer group transition-[right,top] ease-in-out duration-1000 group-hover:top-1/2 group-hover:right-1/2`}
+        className={`flex flex-col align-middle absolute z-10 w-16 right-0 top-0 m-8 cursor-pointer group transition-[right,top] ease-in-out duration-1000 group-hover:top-1/2 group-hover:right-1/2`}
       >
         <img
           className="absolute top-0 right-0 left-0 bottom-0 object-contain opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-100"
@@ -70,7 +70,12 @@ const StarMenu = () => {
         />
 
         {/* I NEED TO DO SOME SERIOUS REFACTORING HERE */}
-        <a href="/watchlist">add to watchlist</a>
+        <a
+          href="/watchlist"
+          className="pt-6 hover:text-purple-400 hover:font-bold opacity-0 transition-opacity duration-1000 ease-out group-hover:opacity-100"
+        >
+          <p>watchlist</p>
+        </a>
       </div>
     </div>
   );
@@ -95,6 +100,9 @@ const StarMenu = () => {
       />
     </>
   );
+
+  //? use this when testing
+  // return <>{StarAnimation}</>;
 
   if (showAnimation) {
     return <>{StarAnimation}</>;
