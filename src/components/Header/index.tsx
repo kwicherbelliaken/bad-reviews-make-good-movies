fa49e6f856ecabbcae1a9b820bd119b42cc50d79
slackermorris <morris.w.jack@gmail.com>
2024-03-01 09:26:01 +1300

    fix: jank on landing page.. content should fit full height

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,7 @@
-//? black: #111111
-
-//? grey: #aaaaaa
-
 const Header = () => {
   return (
-    <div className="bg-[#fafafa] h-24">
-      <div className="bg-[#defe56] p-4 m-4 font-mono700Regular w-fit outline outline-1 outline-[#11111]">
+    <div className="bg-[#fafafa] h-24 p-4">
+      <div className="bg-[#defe56] p-4 font-mono700Regular w-fit outline outline-1 outline-[#11111]">
         bad reviews make good movies
       </div>
     </div>
