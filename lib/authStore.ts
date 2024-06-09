64350da86674e21c2d470b015f2938512517c6d9
slackermorris <morris.w.jack@gmail.com>
2024-06-09 13:02:24 +1200

    refactor: call load on the Clerk client once.. made use of closures and only exposing Clerk logic through private API

--- lib/authStore.ts
+++ lib/authStore.ts
@@ -1,5 +1,42 @@
-// @/lib/authStore.ts
 import { atom } from "nanostores";
-import type { Clerk } from "@clerk/clerk-js";
+import { Clerk } from "@clerk/clerk-js";
 
-export const auth = atom<Clerk | null>(null);
+const clerkPublishableKey = import.meta.env.PUBLIC_CLERK_PUBLISHABLE_KEY;
+
+/**
+ * The principle reponsibility of the store is to keep a reference to the Clerk client.
+ * @returns
+ */
+export const authStore = async () => {
+  const store = atom<Clerk | null>(null);
+  const clerk = new Clerk(clerkPublishableKey);
+
+  await clerk
+    .load()
+    .then(() => {
+      store.set(clerk);
+    })
+    .catch((error) =>
+      console.warn("An error occurred trying to load the Clerk client", {
+        error,
+      })
+    );
+
+  return {
+    get: () => {
+      if (store == null) {
+        console.warn(
+          "Uh oh. The authentication store is not yet instantiated."
+        );
+      }
+
+      if (!clerk.loaded) {
+        console.warn("The Clerk client has not yet loaded.");
+      }
+
+      return store;
+    },
+  };
+};
+
+export const authStoreAPI = await authStore();
