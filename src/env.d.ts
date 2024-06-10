bbe624233a600a859a90c82c8906432e9e2d747a
slackermorris <morris.w.jack@gmail.com>
2024-06-11 07:56:48 +1200

    chore: extended type definition for env vars

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,7 +7,8 @@ interface ImportMetaEnv {
   readonly PUBLIC_TMDB_API_URL: string;
   readonly PUBLIC_WATCHLIST_ID: string;
   readonly PUBLIC_USERNAME: string;
-
+  readonly PUBLIC_CLERK_PUBLISHABLE_KEY: string;
+  readonly CLERK_SECRET_KEY: string;
 }
 
 interface ImportMeta {
