209de6ca21323d8a6b41404a4e7d3e57484ab71e
slackermorris <morris.w.jack@gmail.com>
2024-06-17 19:43:54 +1200

    feature: update add movie to watchlist mutation to dynamically resolve watchlistId

--- src/components/Watchlist/SearchMovie/hooks/mutation.ts
+++ src/components/Watchlist/SearchMovie/hooks/mutation.ts
@@ -2,16 +2,70 @@ import { useMutation } from "@tanstack/react-query";
 import type { BffListResponse } from "../../../../../packages/core/tmdb/types";
 import { queryClient } from "../../query";
 
+import { authStoreAPI } from "../../../../../lib/authStore";
+
+const api = import.meta.env.PUBLIC_API_URL;
+
+const _getUsernameAndWatchlistId = async (): Promise<{
+  username: string;
+  watchlistId: string;
+}> => {
+  // [ ] get the current clerk user
+  let currentUserId;
+
+  const auth = authStoreAPI.get();
+
+  auth.subscribe((clerk) => {
+    if (clerk && clerk.user) {
+      currentUserId = clerk.user.id;
+    }
+  });
+
+  const getUserResponse = await fetch(
+    `${api}/users/${currentUserId}?resolveWatchlist=true`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+
+  const body = await getUserResponse.json();
+
+  if (getUserResponse.status !== 200 || !getUserResponse.ok) {
+    throw new Error(body.error);
+  }
+
+  // Figure out a better way to have type support for my fetch calls.
+  // @ts-ignore
+  return body;
+};
+
 const addMovieToWatchlist = async (payload: any) => {
   const api = import.meta.env.PUBLIC_API_URL;
 
-  const response = await fetch(`${api}/movies/8JWw9ZPsUtkD-14h0Fnzs`, {
+  //! This is so fucking clumsy. Calling '/get-user' each time I add a movie to my watchlist. But, I needed
+  //! a way to resolve the 'watchlistId' which we use in the path.
+  //!
+  //! I settled on modifying the '/get-user' endpoint to return the 'watchlistId' if passed an appropriate flag.
+  //!
+  //! My other alternative was to return it from the `create-user` call and then set it against an equivalent Clerk
+  //! user and thereby sync them up. But then I didn't want to set any more information than I need to into Clerk.
+  //! Watchlist "stuff" feels relevant to my data store. Not Clerks.
+  //!
+  //! I suppose another approach would be to modify the endpoint below to not expect a 'watchlistId' and instead to resolve
+  //! it server side, similar to what I am doing in the '/get-user' endpoint.
+
+  const { username, watchlistId } = await _getUsernameAndWatchlistId();
+
+  const response = await fetch(`${api}/movies/${watchlistId}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify({
-      username: "trial-user",
+      username,
       payload,
     }),
   });
